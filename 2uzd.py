ca27d701efc457aa6c9b35a1a22fb73962c4f53b
Rolands <97937346+rolandslacis@users.noreply.github.com>
2024-05-09 19:19:33 +0300

    Create 2uzd.py

---
+++ 2uzd.py
@@ -0,0 +1,11 @@
+import csv 
+def funkcija():
+    try:
+        with open("fails.csv", "r", encoding="utf-8") as bb:
+            pass
+            
+    except:
+        pass
+if __name__=="__main__":
+  
+    funkcija()
